31ebf2057772d0e61e5867ea2bb142fb5f994deb
Jixin <liujixincd@gmail.com>
2014-10-21 00:50:40 -0700

    Edited the notes

--- Palindrome Number.java
+++ Palindrome Number.java
@@ -1,7 +1,7 @@
 //key idea reverse an integer.
 //time complexity O(n)
 //note: be careful to use long to store the reverse of number x, it might be too big for an integer variable.
-//note2: if x < 0 it cannot be a palindrome.
+//note2: if x < 0 it cannot be a palindrome number.
 public class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) {
