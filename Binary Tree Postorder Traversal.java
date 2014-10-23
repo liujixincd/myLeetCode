fbc6d55cc11b5919a582569dd90150088592bfcd
jixin <liujixincd@gmail.com>
2014-10-22 20:37:42 -0700

    Added new Approach to Solution

--- Binary Tree Postorder Traversal.java
+++ Binary Tree Postorder Traversal.java
@@ -48,4 +48,64 @@ public class Solution {
         return res;
     }
 }
-//iteration without stack
+
+//iteration without stack build a dummy node, left child of the dummy node is root.
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        TreeNode dummy = new TreeNode(0);
+        dummy.left = root;
+        TreeNode cur = dummy;
+        TreeNode pre = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                cur = cur.right;
+            } else {
+                pre = cur.left;
+                while (pre.right != null && pre.right != cur) {
+                    pre = pre.right;
+                }
+                if (pre.right == null) {
+                    pre.right = cur;
+                    cur = cur.left;
+                }
+                if (pre.right == cur) {
+                    reverse(cur.left, pre);
+                    TreeNode temp = pre;
+                    while (temp != cur.left) {
+                        res.add(temp.val);
+                        temp = temp.right;
+                    }
+                    res.add(temp.val);
+                    reverse(pre, cur.left);
+                    pre.right = null;
+                    cur = cur.right;
+                }
+            }
+        }
+        return res;
+    }
+    public void reverse(TreeNode a, TreeNode b) {
+        if (a == b) {
+            return;
+        }
+        TreeNode pre = a;
+        TreeNode cur = pre.right;
+        TreeNode next = null;
+        while (pre != b) {
+            next = cur.right;
+            cur.right = pre;
+            pre = cur;
+            cur = next;
+        }
+    }
+}
